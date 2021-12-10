3ae0361c50cc63616209562c343f9836bd54011a
Ezequiel <ezequielamin@outlook.com>
2021-12-10 01:46:12 -0300

    Todos los productos ahora se muestran en index

--- js/catalogo.js
+++ js/catalogo.js
@@ -2,40 +2,59 @@ let grillaRemeras = document.getElementById("grillaRemeras");
 let grillaPantalones = document.getElementById("grillaPantalones");
 let grillaZapatillas = document.getElementById("grillaZapatillas");
 let grillaAccesorios = document.getElementById("grillaAccesorios");
+let grillaUltimos = document.getElementById("grillaUltimos");
 
-let productos = JSON.parse(localStorage.getItem('listaProductosKey')) || [];
+let productos = JSON.parse(localStorage.getItem("listaProductosKey")) || [];
 let productosRemeras = [];
 let productosPantalones = [];
 let productosZapatillas = [];
 let productosAccesorios = [];
+let i = 0;
 
-productos.forEach(function(producto){
-    if(producto.tipo == "Remera"){
-        productosRemeras.push(producto);
-    }
-    if(producto.tipo == "Pantalon"){
-        productosPantalones.push(producto);
-    }
-    if(producto.tipo == "Zapatilla"){
-        productosZapatillas.push(producto);
-    }
-    if(producto.tipo == "Accesorio"){
-        productosAccesorios.push(producto);
-    }
+productos.forEach(function (producto) {
+  if (producto.categoria == "Remeras") {
+    productosRemeras.push(producto);
+  }
+  if (producto.categoria == "Pantalones") {
+    productosPantalones.push(producto);
+  }
+  if (producto.categoria == "Zapatillas") {
+    productosZapatillas.push(producto);
+  }
+  if (producto.categoria == "Accesorios") {
+    productosAccesorios.push(producto);
+  }
 });
 
+//invertir lista productos para mostrar ultimos ingresos
+productos.reverse();
+for(let i=0;i<4;i++){
+    if(productos[i] == undefined) break;
+    crearCardUltimos(productos[i], grillaUltimos);
+}
+
 //productos.forEach(function(producto){crearCard(producto, grillaRemeras);});
-productosRemeras.forEach(function(producto){crearCard(producto, grillaRemeras);});
-productosPantalones.forEach(function(producto){crearCard(producto, grillaPantalones);});
-productosZapatillas.forEach(function(producto){crearCard(producto, grillaZapatillas);});
-productosAccesorios.forEach(function(producto){crearCard(producto, grillaAccesorios);});
+productosRemeras.forEach(function (producto) {
+  crearCard(producto, grillaRemeras);
+});
+productosPantalones.forEach(function (producto) {
+  crearCard(producto, grillaPantalones);
+});
+productosZapatillas.forEach(function (producto) {
+  crearCard(producto, grillaZapatillas);
+});
+productosAccesorios.forEach(function (producto) {
+  crearCard(producto, grillaAccesorios);
+});
 
 //crear card de ultimos ingresos
 
-function crearCard(producto, grilla){
-    grilla.innerHTML += `<div class="col-sm-12 col-md-4">
+function crearCard(producto, grilla) {
+  grilla.innerHTML += `<div class="col-sm-12 col-md-4">
                             <div class="card">
-                                <img src="${producto.url}" class="card-img-top" alt="${producto.producto}" />
+                                <div class="div-imgCardIndex">
+                                    <img src="${producto.url}" class="card-img-top imgCardIndex" alt="${producto.producto}" />
+                                </div>
                                 <div class="card-body">
                                     <h5 class="card-title fuenteOswald fw-bold">${producto.producto}</h5>
                                     <p class="card-text">${producto.descripcion}</p>
@@ -43,4 +62,24 @@ function crearCard(producto, grilla){
                                 </div>
                             </div>
                         </div>`;
+}
+
+function crearCardUltimos(producto, grilla) {
+  grilla.innerHTML += `<div class="card my-2">
+                        <div class="row" id="grillaUltimos">
+                            <div class="col-4">
+                                <img
+                                src="${producto.url}"
+                                class="card-img-top imgCardsLaterales"
+                                alt="${producto.producto}"
+                                />
+                            </div>
+                            <div class="col-8 px-0">
+                                <div class="card-body px-0">
+                                    <h5 class="card-title fuenteOswald fw-bold">${producto.producto}</h5>
+                                    <p class="card-text fuenteOswald">$ ${producto.precio}</p>
+                                </div>
+                            </div> 
+                        </div>
+                    </div>`;
 }
