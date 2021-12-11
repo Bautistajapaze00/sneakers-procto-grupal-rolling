cd97cd8b53572716f23d49137c9291496b177a3c
Ezequiel <ezequielamin@outlook.com>
2021-12-10 21:56:13 -0300

    Se finaliza busqueda, pagina pendiente a testing

--- js/busqueda.js
+++ js/busqueda.js
@@ -1,67 +1,73 @@
+// llamamos
+let selected_input = document.getElementById("search_input");
+let listaProductos =
+  JSON.parse(localStorage.getItem("listaProductosKey")) || [];
+let grilla = document.getElementById("display-product");
+let grillaSecundaria = document.getElementById("non-display-product");
+let formulario = document.getElementById("formBusqueda");
 
-// llamamos 
-let selected_input = document.getElementById('search_input');
-let listaProductos = JSON.parse(localStorage.getItem('listaProductosKey'))||[];
-let grilla = document.getElementById('display-product');
-let grillaSecundaria = document.getElementById('non-display-product');
+let converter = JSON.parse(localStorage.getItem("searchedProductKey")) || [];
 
-let converter = JSON.parse(localStorage.getItem('searchedProductKey'))||[];
+console.log(converter);
+//busqueda vacia
+if (listaProductos.length == converter.length) {
+  not_defined();
+} else {
+  converter.forEach((producto) => {
+    crearCard(producto);
+  });
+}
 
-if(converter.length>0 ){
-    converter.forEach((producto)=>{draw_cards_index(producto)})
-    }
+limpiarLocalStorage();
 
+function limpiarLocalStorage() {
+  localStorage.removeItem("searchedProductKey");
+}
 
-function draw_cards_index(producto){
-
-    grilla.innerHTML += `<div class="col-sm-12 col-md-4 my-2"> 
-   <div class="card">
-       <div class="div-imgCardIndex">
-           <img src="${producto.url}" class="card-img-top imgCardIndex" alt="${producto.producto}" />
-       </div>    
-       <div class="card-body">
-           <h5 class="card-title fuenteOswald fw-bold">${producto.producto}</h5>
-           <p class="card-text">${producto.descripcion}</p>
-           <p class="text-end mb-0 fw-bold fuenteOswald">$ ${producto.precio}</p>
-       </div>    
-   </div>    
-</div>`   
- }
-
-
-
-
-
-
-
-
-
-
- 
-
+//empiezan funciones de la busqueda de la propia pagina
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    search_product();
+});
 
 // funcion para filtrar los productos del arreglo
-function search_product(){
-grilla.innerHTML = ''
-let selected_product = listaProductos.filter((producto)=> {return producto.descripcion.trim().toLowerCase().includes(selected_input.value.trim().toLowerCase()) || producto.categoria.trim().toLowerCase().includes(selected_input.value.trim().toLowerCase())});
-  
-   console.log(selected_product)
-   if(selected_product.length>0 && selected_input.value.trim().length>0){
-      selected_product.forEach((producto)=>{crearCard(producto)})
-      }
+function search_product() {
+  grilla.innerHTML = "";
+  let selected_product = listaProductos.filter((producto) => {
+    return (
+      producto.producto
+        .trim()
+        .toLowerCase()
+        .includes(selected_input.value.trim().toLowerCase()) ||
+      producto.descripcion
+        .trim()
+        .toLowerCase()
+        .includes(selected_input.value.trim().toLowerCase()) ||
+      producto.categoria
+        .trim()
+        .toLowerCase()
+        .includes(selected_input.value.trim().toLowerCase())
+    );
+  });
 
-   else{
-      not_defined()
-   }
+  if (selected_product.length > 0 && selected_input.value.trim().length > 0) {
+    selected_product.forEach((producto) => {
+      crearCard(producto);
+    });
+  } else {
+    not_defined();
+  }
 
+  limpiarLocalStorage();
 }
 
 // funcion para dibujar cards de productos filtrados
-function crearCard(producto){
- grillaSecundaria.classList.add("d-none")
-   grilla.classList.remove("d-none")
+function crearCard(producto) {
+  grillaSecundaria.classList.add("d-none");
+  grilla.classList.remove("d-none");
 
-   grilla.innerHTML += `<div class="col-sm-12 col-md-4 my-2">
+  grilla.innerHTML += `<div class="col-sm-12 col-md-4 my-2">
    <div class="card">
        <div class="div-imgCardIndex">
            <img src="${producto.url}" class="card-img-top imgCardIndex" alt="${producto.producto}" />
@@ -72,19 +78,15 @@ function crearCard(producto){
            <p class="text-end mb-0 fw-bold fuenteOswald">$ ${producto.precio}</p>
        </div>
    </div>
-</div>`
- }
+</div>`;
+}
 
 // funcion para mostrar en caso de no encontrar los productos filtrados
-function not_defined(){
-    grillaSecundaria.classList.remove("d-none")
-    grillaSecundaria.innerHTML = `<h2> OPPS, parece que ningun producto coincide con tu busqueda</h2>
-    <p>¡No te rindas! Revisa tu ortografía o proba algo menos específico.</p> `
-     grillaSecundaria.className='fuenteOswald mt-5'
-    selected_input.value='';
-    grilla.classList.add('d-none');
-    
-
-
-    
+function not_defined() {
+  grillaSecundaria.classList.remove("d-none");
+  grillaSecundaria.innerHTML = `<h2> OPPS, parece que ningun producto coincide con tu busqueda</h2>
+    <p>¡No te rindas! Revisa tu ortografía o proba algo menos específico.</p> `;
+  grillaSecundaria.className = "fuenteOswald mt-5";
+  selected_input.value = "";
+  grilla.classList.add("d-none");
 }
--- js/busquedaIndex.js
+++ js/busquedaIndex.js
@@ -1,10 +1,39 @@
-let input_index= document.getElementById('search_input_index');
+let input_index = document.getElementById("search_input_index");
 
-let listaProductos = JSON.parse(localStorage.getItem('listaProductosKey'))||[];
-function search_product_index(){
-   
-    let productoBuscado = listaProductos.filter((producto)=> {return producto.descripcion.trim().toLowerCase().includes(input_index.value.trim().toLowerCase()) || producto.categoria.trim().toLowerCase().includes(input_index.value.trim().toLowerCase())}); 
-    console.log(productoBuscado)
-   localStorage.setItem('searchedProductKey',JSON.stringify(productoBuscado));
-    
-}        
+let listaProductos =
+  JSON.parse(localStorage.getItem("listaProductosKey")) || [];
+
+function search_product_index() {
+  let campo = document.getElementById("queryBusqueda");
+  let display = getComputedStyle(campo).display;
+  let input;
+
+  if (display == "none") {
+    input = document.getElementById("search_input_index");
+  } else {
+    input = document.getElementById("search_input_index2");
+  }
+
+  search_product_index_2(input.value);
+}
+
+function search_product_index_2(input) {
+  let productoBuscado = listaProductos.filter((producto) => {
+    return (
+      producto.producto
+        .trim()
+        .toLowerCase()
+        .includes(input.trim().toLowerCase()) ||
+      producto.descripcion
+        .trim()
+        .toLowerCase()
+        .includes(input.trim().toLowerCase()) ||
+      producto.categoria
+        .trim()
+        .toLowerCase()
+        .includes(input.trim().toLowerCase())
+    );
+  });
+
+  localStorage.setItem("searchedProductKey", JSON.stringify(productoBuscado));
+}
